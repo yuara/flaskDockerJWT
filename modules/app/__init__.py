f9fcb77399f74cf2d80063f6991e9fde5b0cd5f8
Yu Arakawa <yuarakawa@Yus-MacBook.local>
2020-04-19 22:19:32 +0900

    Done JWT part in the tutorial

--- modules/app/__init__.py
+++ modules/app/__init__.py
@@ -5,6 +5,8 @@ import datetime
 from bson.objectid import ObjectId
 from flask import Flask
 from flask_pymongo import PyMongo
+from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
 
 
 class JSONEncoder(json.JSONEncoder):
@@ -13,19 +15,23 @@ class JSONEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, ObjectId):
             return str(o)
+        if isinstance(o, set):
+            return list(o)
         if isinstance(o, datetime.datetime):
             return str(o)
+        if isinstance(o, bytes):
+            return o.decode()
         return json.JSONEncoder.default(self, o)
 
 
 # create the flask object
 app = Flask(__name__)
-
-# add mongo url to flask config, so that flask_pymongo can use it to make connection
 app.config['MONGO_URI'] = os.environ.get('DB')
+app.config['JWT_SECRET_KEY'] = os.environ.get('SECRET')
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
 mongo = PyMongo(app)
-
-#use the modified encoder class to handle ObjectId & datetime object while jsonifying the response.
+flask_bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 app.json_encoder = JSONEncoder
 
 from app.controllers import *
